063b9a5345105ddca1199ced85de4b186846807b
Pavel Vaitsikhouski <pavel.vaitsikhouski@schibsted.com>
2020-05-11 12:19:28 +0300

    Initial Commit

---
+++ LikeAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LikeAnimation
+//
+//  Created by Pavel Vaitsikhouski on 5/11/20.
+//  Copyright © 2020 Pavel Vaitsikhouski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
